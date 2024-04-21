8d759977d3590b298cc0ad278f17d3ea028d95bb
f2lpOliveira <contato@fellipefreire.com.br>
2024-04-21 20:17:14 -0300

    feat: Implementação do componente calculadora.

---
+++ projetos/calculadora/src/app/calculadora/components/calculadora.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
